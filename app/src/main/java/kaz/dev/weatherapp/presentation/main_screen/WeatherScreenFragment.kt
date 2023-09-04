a74b3634432a0eccf0978a9e10d0d2bd63f155a4
Dumankrg2019 <52251067+Dumankrg2019@users.noreply.github.com>
2023-09-04 23:47:36 +0600

    feat: add textwatcher

--- app/src/main/java/kaz/dev/weatherapp/presentation/main_screen/WeatherScreenFragment.kt
+++ app/src/main/java/kaz/dev/weatherapp/presentation/main_screen/WeatherScreenFragment.kt
@@ -1,10 +1,17 @@
 package kaz.dev.weatherapp.presentation.main_screen
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.core.os.postDelayed
 import androidx.lifecycle.ViewModelProvider
 import kaz.dev.weatherapp.R
 import kaz.dev.weatherapp.data.ApiService.weatherApiService
@@ -14,6 +21,7 @@ import kaz.dev.weatherapp.utils.API_KEY
 
 class WeatherScreenFragment : Fragment() {
 
+    private val TAG = "weatherScreenFragment"
     private var _binding: FragmentWeatherScreenBinding? = null
     private val binding get() = _binding!!
 
@@ -35,6 +43,8 @@ class WeatherScreenFragment : Fragment() {
 
 
         observeOn()
+
+        checkFieldAndGerQuery(binding.tvCityName)
     }
 
     private fun observeOn(){
@@ -46,6 +56,29 @@ class WeatherScreenFragment : Fragment() {
         })
     }
 
+    private fun checkFieldAndGerQuery(editText: EditText) {
+        val delay = 300L
+        val handler = Handler(Looper.getMainLooper())
+        editText.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+              handler.postDelayed({
+                  if(s.toString().length >= 2) {
+                      viewModel
+                          .fetchWeatherData(
+                              API_KEY,
+                              s.toString(),
+                              "no"
+                          )
+                  }
+              }, delay)
+            }
+
+        })
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
